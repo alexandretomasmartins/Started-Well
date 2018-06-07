26beed3757c12e5b56335368f5c724f3c8287099
alexandretomasmartins <alexandre.martins@hetic.net>
2018-06-07 12:08:18 +0200

    FINAL PUSH YAYAYA

---
+++ data.js
@@ -0,0 +1,115 @@
+let conversation = {
+  question: "Salut, j'ai appris que t'étais mon tuteur 😊",
+  reponses: [
+    {
+      choix: "C'est ça !",
+      complete: "Oui, c'est bien moi ! Je m'apelle Max.",
+      question: "Enchantée. Moi c'est Mary",
+      reponses: [
+        {
+          choix: 'rien',
+          complete: 'Rien lol',
+          question: 'pk rien',
+          reponses: [
+            {
+              choix: 'ba rien',
+              complete: 'je fé rien',
+              question: ' c cool de rien fair ?',
+              reponses: [
+                {
+                  choix: ''
+                }
+              ]
+            },
+          ]
+        },
+        {
+          choix: 'vasy rdv',
+          complete: 'sa te  di 1 ciné',
+          ending: 'tu la pécho gg',
+          dernierMessage: 'jtm'
+        }
+      ]
+    },
+    {
+      choix: "Heu oui c'est moi ^^",
+      complete: "Oui oui, c'est moi. Je suis là si jamais tu as besoin...",
+      question: "Yes, je trouve ça cool d'avoir quelqu'un à qui poser des questions quand on veut !",
+      reponses: [
+        {
+          choix: "Ça c'est sûr!",
+          complete: "Je suis d'accord! Si un jour t'as envie de parler, je suis là.",
+        },
+        {
+          choix: "Ouais c'est plutôt cool.."
+        },
+      ]
+    },
+
+    {
+      choix: 'Heu..',
+      complete: "Heu.. je dois pas être au courant.",
+      question: "Ah... Je pensais que tu l'étais. Je suis nouvelle du coup le proviseur t'a désigné en tant que tuteur pour moi.",
+      reponses: [
+        {
+          choix: ''
+        },
+      ]
+    }
+  ]
+};
+
+const conversElement = document.getElementById('conversation');
+
+game(conversation);
+
+function game(option) {
+  // ON AFFICHE LA QUESTION
+  const message = document.createElement('p');
+  message.classList.add('botMessage');
+  message.innerText = option.question;
+  conversElement.appendChild(message);
+
+  // BOUCLE POUR CHAQUE OPTION POSSIBLE
+  for (let i = 0; i < option.reponses.length; i++) {
+    const possibilite = option.reponses[i];
+
+    // Crée un bouton pour l'option possible
+    const bouton = document.createElement('button');
+    bouton.innerText = possibilite.choix;
+    bouton.className = 'choix';
+
+    // Gestion du click d'une option
+    bouton.addEventListener('click', function(event) {
+      // On affiche la réponse complète du mec
+      const reponse = document.createElement('p');
+      reponse.classList.add('userMessage');
+      reponse.innerText = possibilite.complete;
+      conversElement.appendChild(reponse);
+
+      if (possibilite.ending) {
+        // C FINI
+        // On affiche la dernière réponse de la fille
+        const messageFin = document.createElement('p');
+        messageFin.innerText = possibilite.dernierMessage;
+        conversElement.appendChild(messageFin);
+
+        // On affiche le type de fin au joueur
+        alert(possibilite.ending);
+
+        // on kill la fonction pour finir le jeu avec return;
+        return;
+      } else {
+        // SUPPRIMER LES BOUTONS
+        const boutons = document.querySelectorAll('button.choix');
+        boutons.forEach(function(bouton) {
+          bouton.remove();
+        });
+
+        game(possibilite);
+      }
+    });
+
+    conversElement.appendChild(bouton);
+  }
+}
---
+++ main.js
@@ -0,0 +1,67 @@
+var fadeEffect = document.querySelector('.fadeEffect');
+
+setTimeout( function() {
+    fadeEffect.style.opacity = '0';
+    setTimeout( function() {
+        fadeEffect.style.display = 'none';
+      }, 500);
+  }, 500);
+
+var mockUpOperator = document.querySelector('.mockUpOperator');
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+mockUpOperator.innerHTML += OSName;
+
+var introSection = document.querySelector('.introSection');
+var startButton = document.querySelector('.startButton');
+var mobileContainer = document.querySelector('.mobileContainer');
+var mockUpImage = document.querySelector('.mockUpImage');
+var mockUpMenu = document.querySelector('.mockUpMenu');
+var screenContainer = document.querySelector('.screenContainer');
+var backHomeButtonLink = document.querySelector('.backHomeButtonLink');
+var messageIcon = document.querySelector('.messageIcon');
+var notification = document.querySelector('.notification');
+var messagesContainer = document.querySelector('.messagesContainer');
+var backMenuButton = document.querySelector('.backMenuButton');
+var messageBox = document.querySelectorAll('.messageBox');
+var conversationContainer = document.querySelector('.conversationContainer');
+var backMessagesButton = document.querySelector('.backMessagesButton');
+
+startButton.addEventListener('click', function() {
+  introSection.style.transition = '1s';
+  introSection.style.opacity = '0';
+  mobileContainer.style.display = 'flex';
+  setTimeout( function() {
+      introSection.style.display = 'none';
+      mobileContainer.style.position = 'absolute';
+      mobileContainer.style.top = '50%';
+      mobileContainer.style.transform = 'translateY(-50%)';
+      mockUpImage.classList.add('shadowEffect');
+    }, 1000);
+    messageIcon.addEventListener('click', function() {
+      mockUpMenu.style.display = 'none';
+      notification.style.display = 'none';
+      messagesContainer.style.display = 'block';
+      for (var i = 0; i < messageBox.length; i++) {
+        messageBox[i].addEventListener('click', function() {
+          messagesContainer.style.display = 'none';
+          conversationContainer.style.display = 'block';
+        });
+      }
+      backMessagesButton.addEventListener('click', function() {
+        messagesContainer.style.display = 'block';
+        conversationContainer.style.display = 'none';
+      });
+    });
+});
+
+backHomeButtonLink.addEventListener('click', function() {
+  mobileContainer.style.top = '150vh';
+  setTimeout( function() {
+      window.location.href = "index.html";
+    }, 1000);
+});
